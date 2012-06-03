5462491cefd9ad579e80bdf7f8b4fb8fa4c7866e
Eun-Gyu Kim <eungyu.kim@gmail.com>
2012-06-03 01:06:34 -0700

    separated out config

--- seminar/__init__.py
+++ seminar/__init__.py
@@ -7,17 +7,9 @@ from flask import render_template
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.secret_key = 'dldntEhekfmsdnfl'
-
-app.config.update(
-  DEBUG=True,
-  YEAR=2012,
-  SECRET="gkskslaskfk",
-  SQLALCHEMY_DATABASE_URI='mysql://web:tiffha@localhost/kosta',
-  ADMINPASS="gkskslaskfk"
-)
 db = SQLAlchemy(app)
 
+import seminar.config
 import seminar.views
 
 if __name__ == "__main__":
